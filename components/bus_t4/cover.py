6e7a4cf5acaf00ea5383ee2ce06653e279ae241c
pruwait <pruwait@gmail.com>
2021-12-31 16:50:15 +0300

    Add files via upload

---
+++ components/bus_t4/cover.py
@@ -0,0 +1,30 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import cover
+from esphome.const import CONF_ADDRESS #, CONF_ID, CONF_UPDATE_INTERVAL
+
+#AUTO_LOAD = ['rs485']
+
+bus_t4_ns = cg.esphome_ns.namespace('bus_t4')
+Nice = bus_t4_ns.class_('Nice', cover.Cover, cg.Component)
+
+CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(Nice),
+#    cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
+#    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
+}).extend(cv.COMPONENT_SCHEMA)
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+#    yield rs485.register_rs485_device(var, config)
+    yield cover.register_cover(var, config)
+
+    if CONF_ADDRESS in config:
+        address = config[CONF_ADDRESS]
+        cg.add(var.set_address(address))
+
+    if CONF_UPDATE_INTERVAL in config:
+        update_interval = config[CONF_UPDATE_INTERVAL]
+        cg.add(var.set_update_interval(update_interval))
