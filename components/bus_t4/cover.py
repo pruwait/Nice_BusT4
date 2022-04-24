3c5dbdd09b814066fa02933cab10b86756f5f070
pruwait <pruwait@gmail.com>
2022-04-24 11:17:06 +0300

    Update cover.py

--- components/bus_t4/cover.py
+++ components/bus_t4/cover.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import cover
-from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL
+from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL, CONF_USE_ADDRESS
 
 #AUTO_LOAD = ['rs485']
 
@@ -10,7 +10,8 @@ Nice = bus_t4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
 
 CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Nice),
-#    cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
 #    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
 }).extend(cv.COMPONENT_SCHEMA)
 
@@ -23,8 +24,13 @@ def to_code(config):
 
     if CONF_ADDRESS in config:
         address = config[CONF_ADDRESS]
-        cg.add(var.set_address(address))
+        cg.add(var.set_to_address(address))
 
+    if CONF_USE_ADDRESS in config:
+        use_address = config[CONF_USE_ADDRESS]
+        cg.add(var.set_from_address(use_address))
+        
+        
     if CONF_UPDATE_INTERVAL in config:
         update_interval = config[CONF_UPDATE_INTERVAL]
         cg.add(var.set_update_interval(update_interval))
