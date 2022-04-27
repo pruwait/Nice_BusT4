0060d465eb6a95ce3622c601638a31b6aecada1c
pruwait <pruwait@gmail.com>
2022-04-27 13:24:39 +0300

    Update nice-bust4.h

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -429,6 +429,7 @@ class NiceBusT4 : public Component, public Cover {
     uint16_t from_addr = 0x0066; //от кого пакет, адрес bust4 шлюза
     uint16_t to_addr = 0x00ff;	 // кому пакет, адрес контроллера привода, которым управляем
     uint16_t oxi_addr = 0x000a;	 // адрес приемника
+    uint8_t whose_byte;	 // Байт для произвольных команд GET/SET
 
 	
 	
