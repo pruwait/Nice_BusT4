2e00f83cd38b3a06b460a08800a43f3e2d61c8cc
pruwait <pruwait@gmail.com>
2022-04-28 20:30:33 +0300

    Update nice-bust4.cpp

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -370,6 +370,19 @@ void NiceBusT4::parse_status_packet (const std::vector<uint8_t> &data) {
           //          ESP_LOGCONFIG(TAG, "  Прошивка: %S ", str.c_str());
           this->firmware_.assign(this->rx_message_.begin() + 14, this->rx_message_.end() - 2);
           break;
+        case WHO:
+          //          ESP_LOGCONFIG(TAG, "  Прошивка: %S ", str.c_str());
+          if (data[12]==0x01) {
+            if (data[14]==0x04) { // привод
+              this-> to_addr = ((uint16_t)data[5] << 8) | data[5];
+            }
+            else if (data[14]==0x0A){ // приёмник
+              this-> oxi_addr = ((uint16_t)data[5] << 8) | data[5];
+            }
+          }
+          
+          //this->firmware_.assign(this->rx_message_.begin() + 14, this->rx_message_.end() - 2);
+          break;          
       }  // switch
 
     }  // if  ROOT ответы на запросы GET, пришедшие без ошибок
@@ -641,8 +654,9 @@ void NiceBusT4::dump_config() {    //  добавляем в  лог инфор�
   std::string firm_str(this->firmware_.begin(), this->firmware_.end());
   ESP_LOGCONFIG(TAG, "  Прошивка: %S ", firm_str.c_str());
 
-  ESP_LOGCONFIG(TAG, "  Ряд (серия) шлюза: %x адрес шлюза: %x", (uint8_t)(this->from_addr >> 8), (uint8_t)(this->from_addr & 0xFF));
-  ESP_LOGCONFIG(TAG, "  Ряд (серия) привода: %x адрес привода: %x", (uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF));
+  ESP_LOGCONFIG(TAG, "  Адрес шлюза: %x%x", (uint8_t)(this->from_addr >> 8), (uint8_t)(this->from_addr & 0xFF));
+  ESP_LOGCONFIG(TAG, "  Адрес привода: %x%x", (uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF));
+  ESP_LOGCONFIG(TAG, "  Адрес приёмника: %x%x", (uint8_t)(this->oxi_addr >> 8), (uint8_t)(this->oxi_addr & 0xFF));
 }
 
 
