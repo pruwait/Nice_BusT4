c3f6ecea26ff7817015e595b64c2767d5d67d8d4
pruwait <pruwait@gmail.com>
2023-10-18 19:10:13 +0300

    Update nice-bust4.cpp

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -75,6 +75,7 @@ void NiceBusT4::setup() {
   //  this->last_init_command_ = 0;
   // кто в сети?
 //  this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));
+  
 
 }
 
@@ -83,8 +84,10 @@ void NiceBusT4::loop() {
     if ((millis() - this->last_update_) > 10000) {    // каждые 10 секунд
 // если привод не определился с первого раза, попробуем позже
         std::vector<uint8_t> unknown = {0x55, 0x55};
-        if (this->init_ok == false) 
-          {this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));}
+        if (this->init_ok == false) {
+          this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));
+          this->tx_buffer_.push(gen_inf_cmd(addr1, addr2, FOR_ALL, PRD, GET, 0x00)); //запрос продукта
+        }
         
         else if (this->class_gate_ == 0x55) init_device((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), 0x04);  
         else if (this->manufacturer_ == unknown)  {
@@ -290,7 +293,12 @@ void NiceBusT4::parse_status_packet (const std::vector<uint8_t> &data) {
         //положение максимального открытия энкодера, открытия, закрытия
 
         case MAX_OPN:
-          this->_max_opn = (data[14] << 8) + data[15];
+          if (is_walky) {
+            this->_max_opn = data[15];
+          }
+          else {  
+            this->_max_opn = (data[14] << 8) + data[15];
+          }
           ESP_LOGI(TAG, "Максимальное положение энкодера: %d", this->_max_opn);
           break;
 
